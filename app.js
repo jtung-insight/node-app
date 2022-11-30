38540c1d310c13a0a81489320abb1a6439fe8eaa
jtung-insight <jtung@insight.com>
2022-11-30 15:23:27 -0800

    ETA

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Argo CD! Is this really working? Lets try again! We go again!! And again!!'))
+app.get('/', (req, res) => res.send('Hello Argo CD! Is this really working? Lets try again! We go again!! And again!! ETA?'))
 app.listen(3000, () => console.log('Server ready'))
