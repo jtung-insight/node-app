f1815f3668ba21ff0f39e9da6f261338f9339698
jtung-insight <jtung@insight.com>
2022-11-30 10:13:46 -0800

    And again

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Argo CD! Is this really working? Lets try again! We go again!!'))
+app.get('/', (req, res) => res.send('Hello Argo CD! Is this really working? Lets try again! We go again!! And again!!'))
 app.listen(3000, () => console.log('Server ready'))
